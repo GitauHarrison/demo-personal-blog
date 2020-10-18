8f9c1f92212718e8ec700bf638a2cb3fd3daa05d
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-18 07:13:38 +0300

    Add comments table

--- app/models.py
+++ app/models.py
@@ -8,3 +8,12 @@ class User(db.Model):
 
     def __repr__(self):
         return 'User <>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(500))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow) 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return 'Post <>'.format(self.body)
