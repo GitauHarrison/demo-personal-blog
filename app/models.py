f87f03aed902dfe7be9e32a232b28c96692c291b
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-18 07:16:20 +0300

    Add user-post relationship

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return 'User <>'.format(self.username)
