2be361218ffbcfc030d3312ab6cdbb587d365d5f
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-18 04:12:52 +0300

    Add hire page

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,7 @@ from flask import render_template, url_for
 @app.route('/about_me')
 def about_me():
     return render_template('about_me.html', title = 'About Me')
+
+@app.route('/hire_me')
+def hire_me():
+    return render_template('hire_me.html', title = 'Hire Me')
