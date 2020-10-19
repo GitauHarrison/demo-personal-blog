89e1bdf8015ae69afc8ac787baf4f49837eb6096
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-19 10:58:18 +0300

    Remove user validation

--- app/forms.py
+++ app/forms.py
@@ -4,17 +4,17 @@ from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
 class CommentForm(FlaskForm):
-    usernme = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     comment = TextAreaField('Comment', validators=[DataRequired()])
     submit = SubmitField('Post')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different username.')
+    # def validate_username(self, username):
+    #     user = User.query.filter_by(username=username.data).first()
+    #     if user is not None:
+    #         raise ValidationError('Please use a different username.')
 
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different email address.')
+    # def validate_email(self, email):
+    #     user = User.query.filter_by(email=email.data).first()
+    #     if user is not None:
+    #         raise ValidationError('Please use a different email address.')
