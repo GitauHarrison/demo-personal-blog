a2fb88c2b1fd4b6efb8527295cfadde93e650740
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-20 20:43:18 +0300

    Add route

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, url_for, flash, redirect, request
 from app.forms import CommentForm
 from app.models import User, Post
 
+@app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title = 'Home')
