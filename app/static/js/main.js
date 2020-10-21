e0c72883ae91a68b568cea7e58e07869fb463e7a
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-21 12:32:05 +0300

    Make AJAX request to config route using FETCH API

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,1 +1,8 @@
 console.log("Sanity check!") // this is just a printout
+
+fetch("/config")
+.then((result) => { return result.json(); })
+.then((data) => {
+  // Initialize Stripe.js
+  const stripe = Stripe(data.publicKey);
+});
