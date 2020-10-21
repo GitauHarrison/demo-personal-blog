eafdf288e716288d3aa0790d657df1a180898532
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-21 12:35:22 +0300

    Solve the result.json promise

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,24 @@
 console.log("Sanity check!") // this is just a printout
 
+// Get Stripe publishable key
 fetch("/config")
 .then((result) => { return result.json(); })
 .then((data) => {
   // Initialize Stripe.js
   const stripe = Stripe(data.publicKey);
+
+  // Add Event handler
+  document.querySelector("#submitBtn").addEventListener("click", () => {
+    // Get Checkout Session ID
+    fetch("/create-checkout-session")
+    .then((result) => { return result.json(); })
+    .then((data) => {
+      console.log(data);
+      // Redirect to Stripe Checkout
+      return stripe.redirectToCheckout({sessionId: data.sessionId})
+    })
+    .then((res) => {
+      console.log(res);
+    });
+  });
 });
