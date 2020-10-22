d65fff26592686c83f25d59210706e2d8d72aaa3
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-22 04:59:21 +0300

    Add comment form

--- app/routes.py
+++ app/routes.py
@@ -147,10 +147,50 @@ def translate_text():
 # END OF LIVE TRANSLATION
 
 # START OF TUTORIALS
-@app.route('/virtualenvwrapper')
+@app.route('/virtualenvwrapper', methods = ['GET', 'POST'])
 def virtualenvwrapper():
-    return render_template('virtualenvwrapper.html', title = 'Virtualenvwrapper')
+    form = CommentForm()
+    if form.validate_on_submit():
+        language = guess_language(form.comment.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        user = User(username = form.username.data, email = form.email.data)        
+        post = Post(body = form.comment.data, author = user, language = language)
+        db.session.add(user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your comment is now live!')  
+        return redirect(url_for('virtualenvwrapper', _anchor='comments'))  
+    page = request.args.get('page', type = int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('virtualenvwrapper', _anchor='comments', page = posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('virtualenvwrapper', _anchor='comments', page = posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('virtualenvwrapper.html', title = 'Virtualenvwrapper', form = form, posts = posts.items, next_url = next_url, prev_url = prev_url)
 
-@app.route('/vagrant')
+@app.route('/vagrant', methods = ['GET', 'POST'])
 def vagrant():
-    return render_template('vagrant.html', title = 'Vagrant')
+    form = CommentForm()
+    if form.validate_on_submit():
+        language = guess_language(form.comment.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        user = User(username = form.username.data, email = form.email.data)        
+        post = Post(body = form.comment.data, author = user, language = language)
+        db.session.add(user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your comment is now live!')  
+        return redirect(url_for('vagrant', _anchor='comments'))  
+    page = request.args.get('page', type = int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('vagrant', _anchor='comments', page = posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('vagrant', _anchor='comments', page = posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('vagrant.html', title = 'Vagrant', form = form, posts = posts.items, next_url = next_url, prev_url = prev_url)
