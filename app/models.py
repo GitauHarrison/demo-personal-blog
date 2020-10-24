d1042330bbf0d8147567be0e2742dcd7ba141db3
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-24 03:59:16 +0300

    Handle rich text in server

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from app import db
 from datetime import datetime
 from hashlib import md5
+from markdown import markdown
+import bleach
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -41,12 +43,24 @@ class VagrantPost(db.Model):
 class VirtualenvwrapperPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(500))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow) 
+    body_html = db.Column(db.String(500))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)     
+    language = db.Column(db.String(5))    
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    language = db.Column(db.String(5))
 
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = [
+            'a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+            'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+            'h1', 'h2', 'h3', 'p'
+        ]
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format = 'html'), tags = allowed_tags, strip = True))
+        
     def __repr__(self):
         return 'Post <>'.format(self.body)
+        
+db.event.listen(VirtualenvwrapperPost.body, 'set', VirtualenvwrapperPost.on_changed_body)
 
 class reCaptchaPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
