d09c58df9bca01f50f6824c2f92f73fc71a7a163
Gitau Harrison <harrisongitau3@gmail.com>
2020-11-14 21:27:47 +0300

    Add Flask web forms content

--- blog.py
+++ blog.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from app.models import User, PersonalBlogPost, VagrantPost, VirtualenvwrapperPost, reCaptchaPost, richTextPost, ngrokPost, installDocker, HerokuDeployment, \
-     WebDevelopmentPost, HelloWorldPost, FlaskTemplatesPost
+     WebDevelopmentPost, HelloWorldPost, FlaskTemplatesPost, FlaskWebFormsPost
 
 app = create_app()
 
@@ -8,5 +8,6 @@ app = create_app()
 def make_shell_context():
     return {'db': db, 'User': User, 'PersonalBlogPost': PersonalBlogPost, 'VagrantPost': VagrantPost, 'VirtualenvwrapperPost': VirtualenvwrapperPost, \
          'reCaptchaPost': reCaptchaPost, 'richTextPost': richTextPost, 'ngrokPost': ngrokPost, 'installDocker': installDocker, \
-              'HerokuDeployment': HerokuDeployment, 'WebDevelopmentPost': WebDevelopmentPost, 'HelloWorldPost': HelloWorldPost, 'FlaskTemplatesPost': FlaskTemplatesPost
+              'HerokuDeployment': HerokuDeployment, 'WebDevelopmentPost': WebDevelopmentPost, 'HelloWorldPost': HelloWorldPost, \
+                   'FlaskTemplatesPost': FlaskTemplatesPost, 'FlaskWebFormsPost': FlaskWebFormsPost
     }
