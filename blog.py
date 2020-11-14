5ed7ee473bf286ee00403879d9fc025cde70bf99
Gitau Harrison <harrisongitau3@gmail.com>
2020-11-14 18:54:33 +0300

    Add Flask Templates tutorial

--- blog.py
+++ blog.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from app.models import User, PersonalBlogPost, VagrantPost, VirtualenvwrapperPost, reCaptchaPost, richTextPost, ngrokPost, installDocker, HerokuDeployment, \
-     WebDevelopmentPost, HelloWorldPost
+     WebDevelopmentPost, HelloWorldPost, FlaskTemplatesPost
 
 app = create_app()
 
@@ -8,5 +8,5 @@ app = create_app()
 def make_shell_context():
     return {'db': db, 'User': User, 'PersonalBlogPost': PersonalBlogPost, 'VagrantPost': VagrantPost, 'VirtualenvwrapperPost': VirtualenvwrapperPost, \
          'reCaptchaPost': reCaptchaPost, 'richTextPost': richTextPost, 'ngrokPost': ngrokPost, 'installDocker': installDocker, \
-              'HerokuDeployment': HerokuDeployment, 'WebDevelopmentPost': WebDevelopmentPost, 'HelloWorldPost': HelloWorldPost
+              'HerokuDeployment': HerokuDeployment, 'WebDevelopmentPost': WebDevelopmentPost, 'HelloWorldPost': HelloWorldPost, 'FlaskTemplatesPost': FlaskTemplatesPost
     }
