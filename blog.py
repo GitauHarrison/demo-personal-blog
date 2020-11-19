e27a6959bf9b3b89d7840ed1907da982f6767508
Gitau Harrison <harrisongitau3@gmail.com>
2020-11-19 13:44:51 +0300

    Add web development table

--- blog.py
+++ blog.py
@@ -1,13 +1,15 @@
 from app import create_app, db
-from app.models import User, PersonalBlogPost, VagrantPost, VirtualenvwrapperPost, reCaptchaPost, richTextPost, ngrokPost, installDocker, HerokuDeployment
+from app.models import User, PersonalBlogPost, VagrantPost, VirtualenvwrapperPost, reCaptchaPost, richTextPost, ngrokPost, installDocker, HerokuDeployment,\
+     WebDevelopmentPost
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
      return {'db': db, 'User': User, 'PersonalBlogPost': PersonalBlogPost, 'VagrantPost': VagrantPost, 'VirtualenvwrapperPost': VirtualenvwrapperPost, \
-          'reCaptchaPost': reCaptchaPost, 'richTextPost': richTextPost, 'ngrokPost': ngrokPost, 'installDocker': installDocker, 'HerokuDeployment': HerokuDeployment
+          'reCaptchaPost': reCaptchaPost, 'richTextPost': richTextPost, 'ngrokPost': ngrokPost, 'installDocker': installDocker, 'HerokuDeployment': HerokuDeployment,\
+               'WebDevelopmentPost': WebDevelopmentPost
     }
 
-#               , 'WebDevelopmentPost': WebDevelopmentPost, 'HelloWorldPost': HelloWorldPost, \
+#               , 'HelloWorldPost': HelloWorldPost, \
 #                    'FlaskTemplatesPost': FlaskTemplatesPost, 'FlaskWebFormsPost': FlaskWebFormsPost
