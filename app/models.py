ee113bf1c463dc1652f4474d4443b3cf740bfa77
Gitau Harrison <harrisongitau3@gmail.com>
2020-11-19 13:10:56 +0300

    Add virtualenvwrapper table

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True)
     posts = db.relationship('PersonalBlogPost', backref='author', lazy='dynamic')
     vagrant_posts = db.relationship('VagrantPost', backref='author', lazy='dynamic')
+    virtualenvwrapper_posts = db.relationship('VirtualenvwrapperPost', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return 'User <>'.format(self.username)
@@ -61,3 +62,25 @@ class VagrantPost(db.Model):
         return 'Post <>'.format(self.body)
         
 db.event.listen(VagrantPost.body, 'set', VagrantPost.on_changed_body)
+
+class VirtualenvwrapperPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(500))
+    body_html = db.Column(db.String(500))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)     
+    language = db.Column(db.String(5))    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = [
+            'a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+            'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+            'h1', 'h2', 'h3', 'p'
+        ]
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format = 'html'), tags = allowed_tags, strip = True))
+        
+    def __repr__(self):
+        return 'Post <>'.format(self.body)
+        
+db.event.listen(VirtualenvwrapperPost.body, 'set', VirtualenvwrapperPost.on_changed_body)
--- blog.py
+++ blog.py
@@ -1,14 +1,14 @@
 from app import create_app, db
-from app.models import User, PersonalBlogPost, VagrantPost
+from app.models import User, PersonalBlogPost, VagrantPost, VirtualenvwrapperPost
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-     return {'db': db, 'User': User, 'PersonalBlogPost': PersonalBlogPost, 'VagrantPost': VagrantPost
+     return {'db': db, 'User': User, 'PersonalBlogPost': PersonalBlogPost, 'VagrantPost': VagrantPost, 'VirtualenvwrapperPost': VirtualenvwrapperPost
     }
 
-#     , 'VirtualenvwrapperPost': VirtualenvwrapperPost, \
+#     , \
 #          'reCaptchaPost': reCaptchaPost, 'richTextPost': richTextPost, 'ngrokPost': ngrokPost, 'installDocker': installDocker, \
 #               'HerokuDeployment': HerokuDeployment, 'WebDevelopmentPost': WebDevelopmentPost, 'HelloWorldPost': HelloWorldPost, \
 #                    'FlaskTemplatesPost': FlaskTemplatesPost, 'FlaskWebFormsPost': FlaskWebFormsPost
