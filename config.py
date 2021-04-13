dcfdcd9c0d5069cc7f744749a366b5eb1b3142db
harry <harrisongitau3@gmail.com>
2021-04-13 06:29:56 +0300

    Fix type error between str and int

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
-# load_dotenv()
 
 
 class Config(object):
@@ -16,26 +15,36 @@ class Config(object):
     START_NGROK = os.environ.get('START_NGROK')
 
     # Form security
-    SECRET_KEY=b'\xfa\x1c\x03q\xe4I\xd8\xa3\x16\xc0\xca\x1e\xe8wf\x8d'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    POSTS_PER_PAGE=20
+    POSTS_PER_PAGE = int(os.environ.get('POSTS_PER_PAGE'))
 
-    MAIL_SERVER='smtp.gmail.com'
-    MAIL_PORT='587'
-    MAIL_USE_TLS='True'
-    MAIL_USERNAME='tastebolder@gmail.com'
-    MAIL_PASSWORD=b'\x03K\xec\x02\xf2-\xdd/\xd1\xa2\xc4N\x89\xcf~\x82'
-    ADMINS=['norulesanymore@gmail.com']
+    # Email configuration
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT ')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = os.environ.get('ADMINS')
 
-    STRIPE_PUBLISHABLE_KEY='pk_test_51HedKjKExJxw7LdpAQR3oWJ1AWW94e8leKDmjbPkLbjvW81NzX2jXntzt774lnSxiuubyBItFVPUmeVUsxBWaoNH00eLRpQz3v'
-    STRIPE_SECRET_KEY='sk_test_51HedKjKExJxw7LdpBoS4e5v2Bekry7MvxSCXjKKBjchSe59n9KADKBJzwiFqLxIFEstnY8V1Mn6BYm00hQET99Wl00lEhu0Lj5'
-    STRIPE_ENDPOINT_SECRET='whsec_wnJMeZIAWSHZm5sONh5eBP8pSmiWpcDp'
+    # Stripe
+    STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_PUBLISHABLE_KEY')
+    STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+    STRIPE_ENDPOINT_SECRET = os.environ.get('STRIPE_ENDPOINT_SECRET')
 
-    MS_TRANSLATOR_KEY='344435f010f8410caaa6c1f972507a3e'
+    # MS Live Translation
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
 
-    LANGUAGES=['en', 'sw']
+    # Manual Translation Language Options
+    LANGUAGES = os.environ.get('LANGUAGES')
 
-    RECAPTCHA_PUBLIC_KEY='6LfLJN8ZAAAAAJgBHQy76zPyHVR2O9jmaTThPTLP'
-    RECAPTCHA_PRIVATE_KEY='6LfLJN8ZAAAAACe28iK98cDOBl-1REobjjR-em1a'
+    # reCAPTCHA configuration
+    RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
+    RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
 
-    LOG_TO_STDOUT=1
+    # Ngrok configuration
+    START_NGROK = os.environ.get('START_NGROK') is not None and \
+        os.environ.get('WERKZEUG_RUN_MAIN') is not 'true'
+
+    # Heroku logs requirement
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
