08172dc51b22720663eaf4c1498bd6f1c7720632
harry <harrisongitau3@gmail.com>
2021-05-12 16:31:32 +0300

    Development: Add comment moderation to flask database article

--- app/main/email.py
+++ app/main/email.py
@@ -80,6 +80,32 @@ def send_live_flask_web_forms_email(user):
     )
 
 
+# Flask Database
+
+def new_flask_database_comment(admin):
+    send_email(
+        '[New Comment] Flask Database Article',
+        sender=current_app.config['ADMINS'][0],
+        recipients=[admin.email],
+        text_body=render_template('admin/review_comment_email/flask_database.txt',
+                                  admin=admin),
+        html_body=render_template('admin/review_comment_email/flask_database.html',
+                                  admin=admin)
+    )
+
+
+def send_live_flask_database_email(user):
+    send_email(
+        '[Your Comment is Live] Flask Database Article',
+        sender=current_app.config['ADMINS'][0],
+        recipients=[user.email],
+        text_body=render_template('public_comment_email/flask_database.txt',
+                                  user=user),
+        html_body=render_template('public_comment_email/flask_database.html',
+                                  user=user)
+    )
+
+
 # Virtualeenvwrapper
 
 def new_virtualenvwrapper_comment(admin):
--- app/main/routes.py
+++ app/main/routes.py
@@ -25,7 +25,8 @@ from app.main.email import new_twilio_sendgrid_comment,\
     send_live_virtualenvwrapper_email, new_hello_world_comment,\
     send_live_hello_world_email, new_flask_templates_comment,\
     send_live_flask_templates_email, new_flask_web_forms_comment,\
-    send_live_flask_web_forms_email
+    send_live_flask_web_forms_email, new_flask_database_comment,\
+    send_live_flask_database_email
 
 
 @bp.route('/')
@@ -299,6 +300,33 @@ def review_flask_web_forms_comments():
                            )
 
 
+# Flask Database
+
+
+@bp.route('/admin/blog-review/personal-blog/chapter-4/flask-database')
+@login_required
+def review_flask_database_comments():
+    page = request.args.get('page', 1, type=int)
+    comments = FlaskDatabasePost.query.order_by(
+        FlaskDatabasePost.timestamp.desc()).paginate(
+            page, current_app.config['POSTS_PER_PAGE'], False
+        )
+    next_url = url_for('main.review_flask_database_comments',
+                       page=comments.next_num) \
+        if comments.has_next else None
+    prev_url = url_for('main.review_flask_database_comments',
+                       page=comments.prev_num) \
+        if comments.has_prev else None
+    all_comments = FlaskDatabasePost.query.all()
+    total_comments = len(all_comments)
+    return render_template('admin/comment_moderation/review_flask_database.html',
+                           title='Review Flask Database',
+                           comments=comments.items,
+                           next_url=next_url,
+                           prev_url=prev_url,
+                           total_comments=total_comments
+                           )
+
 # Twilio SendGrid
 
 
@@ -508,6 +536,19 @@ def allow_flask_web_forms_comment(id):
         send_live_flask_web_forms_email(user)
     return redirect(url_for('main.review_flask_web_forms_comments'))
 
+
+@bp.route('/allow-flask-database-comment/<id>')
+def allow_flask_database_comment(id):
+    post = FlaskDatabasePost.query.get(id)
+    post.allowed_comment = 1
+    db.session.add(post)
+    db.session.commit()
+    flash(f"Comment {post.id} allowed. See Flask Database article")
+    user = User.query.get(id)
+    if user:
+        send_live_flask_database_email(user)
+    return redirect(url_for('main.review_flask_database_comments'))
+
 # ---
 # End of Personal Blog Articles
 # ---
@@ -624,6 +665,16 @@ def delete_flask_web_forms_comment(id):
     db.session.commit()
     flash(f'Successfully deleted comment {post.id} in Flask Web Forms article')
     return redirect(url_for('main.review_flask_web_forms_comments'))
+
+
+@bp.route('/delete-flask-database-comment/<id>')
+def delete_flask_database_comment(id):
+    post = FlaskDatabasePost.query.get(id)
+    db.session.delete(post)
+    db.session.commit()
+    flash(f'Successfully deleted comment {post.id} in Flask Database article')
+    return redirect(url_for('main.review_flask_database_comments'))
+
 # ---
 # End of Personal Blog Articles
 # ---
@@ -1062,7 +1113,11 @@ def flask_database():
         db.session.add(post)
         db.session.commit()
         flash('You will receive an email when your comment is live!')
+        admins = Admin.query.all()
+        for admin in admins:
+            new_flask_database_comment(admin)
         return redirect(url_for('main.flask_database', _anchor='comments'))
+    all_allowed_comments = FlaskDatabasePost.query.filter_by(allowed_comment=1).all()
     page = request.args.get('page', 1, type=int)
     posts = FlaskDatabasePost.query.order_by(
         FlaskDatabasePost.timestamp.asc()).paginate(
@@ -1076,15 +1131,15 @@ def flask_database():
                        _anchor='comments',
                        page=posts.prev_num) \
         if posts.has_prev else None
-    all_posts = FlaskDatabasePost.query.all()
-    total = len(all_posts)
+    total = len(all_allowed_comments)
     return render_template('personal_blog_templates/working_with_database.html',
                            title='Database',
                            form=form,
                            posts=posts.items,
                            next_url=next_url,
                            prev_url=prev_url,
-                           total=total
+                           total=total,
+                           all_allowed_comments=all_allowed_comments
                            )
 
 
